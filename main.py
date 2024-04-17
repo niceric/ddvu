fe554181dc42587291eede3b122ef5e0736ab009
niceric <niclasericsson89@hotmail.com>
2024-04-17 23:11:24 +0200

    added some more code for exploring the dataset

--- main.py
+++ main.py
@@ -31,6 +31,26 @@ print(df[df["headline"].isin(yrken)])
 print("-------------------------- DISPLAY ROWS WHERE THE COLUMN CONTAINS A SPECIFIC STRING ('sjuk') '.str.contains()' --------------------------")
 print(df[df["headline"].str.contains("sjuk")])
 
+print("-------------------------- DISPLAY SELECTED ROWS FROM A FILTERED DF ('sjuk') '.str.contains()' --------------------------")
+sorterade = df[df["headline"].str.contains("sjuk")]
+    # axis = 1 för att filtrera bland kolumner, 0 för rader 
+print(sorterade.filter(items = ["working_hours_type", "headline"], axis = 1))
+
+
+print("-------------------------- DISPLAY SELECTED ROWS FROM A FILTERED DF AND ONLY SHOWS 'last_publication_date' after 2006-12-31  ('sjuk') '.str.contains()' --------------------------")
+print(sorterade[sorterade["last_publication_date"] > "2006-12-31"])
+    # sort_values gör att man kan sortera för värden inom en kolumn, här med datum
+    # för mer info om sortering se: https://youtu.be/kB7FV-ijdqE?si=B8kDfABae9009dXR&t=562
+print(sorterade[sorterade["last_publication_date"] > "2006-12-31"].sort_values(by = "last_publication_date"))
+
+
+print("-------------------------- SORT AND FOR SJUK AND SETS THE INDEX AS EMPLOYER --------------------------")
+sorterade_2 = df[df["headline"].str.contains("sjuk")]
+indexerat = sorterade_2.set_index("employer")
+print(indexerat)
+
+
+
 
 
 
