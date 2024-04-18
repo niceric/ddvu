6c4ffec19a736b1d664eb87f8661394edcc56f17
niceric <niclasericsson89@hotmail.com>
2024-04-18 22:47:09 +0200

    fortsatt utforska pandas och datasetet, ändrade till pd.json_normalize för att flatten dataframet

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import pandas as pd
 import json
 
 pd.set_option("display.max.rows", 1000, "display.max.columns", 36)
-df = pd.read_json(r"dataset/subset.json")
+with open('dataset/subset.json', 'r') as file: data = json.load(file)
+df = pd.json_normalize(data)
+
+
+#df = pd.read_json(r"dataset/subset.json")
+#df = df.json_normalize(df)
+
 #print(df)
 #with pd.option_context('display.max_rows', None, 'display.max_columns', None):
 #    print(df.head(3))
@@ -44,13 +50,36 @@ print(sorterade[sorterade["last_publication_date"] > "2006-12-31"])
 print(sorterade[sorterade["last_publication_date"] > "2006-12-31"].sort_values(by = "last_publication_date"))
 
 
-print("-------------------------- SORT AND FOR SJUK AND SETS THE INDEX AS EMPLOYER --------------------------")
+#print("-------------------------- SORT FOR SJUK AND SETS THE INDEX AS EMPLOYER --------------------------")
+
+#indexerat = sorterade_2.set_index("employer")
+#print(indexerat)
+
+
+# Skriver ut annonserna med index 0, 1, 2 utifrån den sorterade och indexerade listan ovan
 sorterade_2 = df[df["headline"].str.contains("sjuk")]
-indexerat = sorterade_2.set_index("employer")
-print(indexerat)
+print(sorterade_2.iloc[1], sorterade_2.iloc[0], sorterade_2.iloc[2])
 
 
 
+# Räknar ut antalet värden inom en column, label, och skriver ut hur många det finns av varje
+working_hours_type = df["working_hours_type.label"]
+print(working_hours_type.value_counts())
+
+# Skriver ut all data för kolumnen working_hours_type där "sjuk" omnämns i headline  
+sorterade_2 = df[df["headline"].str.contains("sjuk")]
+test_1 = sorterade_2["working_hours_type.label"]
+print(test_1)
+print(test_1.value_counts())
+
+# gör om ovanstående till en lista och skriver endast ut "label" för denna kolumns data
+list_of_working_hours = test_1.tolist()
+print(list_of_working_hours)
+#for listing in list_of_working_hours:
+#    print()
+
+print(len(list_of_working_hours))
+#print(df)
 
 
 
