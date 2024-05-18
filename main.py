82a42414b307527eb9a5c1755cb6b25c8d954bd0
niceric <niclasericsson89@hotmail.com>
2024-05-18 22:54:06 +0200

    fixed the sorting algorithm for most occuring words

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 import pandas as pd
-import json
+from operator import itemgetter
+import re
 
+# Lista med gemensamma nyckelord för vården
+gemensamma_keywords = [
+    "Sjuksköterska",
+    "Specialistsjuksköterska",
+    "Legitimerad sjuksköterska",
+    "Undersköterska",
+    "Vårdassistent",
+    "Vårdbiträde",
+    "Barnmorska",
+    "Vårdpersonal",
+    "Omvårdnad",
+    "Sjukvård",
+    "Akutsjukvård",
+    "Primärvård",
+    "Intensivvård",
+    "Omvårdnadspersonal",
+    "Vårdområdet",
+    "Omsorgsarbetare",
+    "Vårdsamordnare",
+    "Äldreomsorg",
+    "Hemtjänst",
+    "Gynekologi",
+    "Gynekologisk vård",
+    "Graviditetsvård",
+    "Förlossning",
+    "Mödra- och barnhälsovård",
+    "Förlossningsavdelning"
+]
 
 with open("stopwords.txt", "r") as file:
     stopwords = file.read().replace("\n", " ")
@@ -44,133 +73,69 @@ def get_list_of_words(text):
         return x
 
 
-
-
-#pd.set_option("display.max.rows", 1000, "display.max.columns", 36)
-#with open('subset_with_keywords2023.csv', 'r') as file: 
-#    data = json.load(file)
 df = pd.read_csv("dataset/subset_with_keywords2023.csv")
 
-#df = pd.read_json(r"dataset/subset.json")
-#df = df.json_normalize(df)
 
-#print(df)
-#with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-#    print(df.head(3))
-print("-------------------------- INFO --------------------------")
-df.info()
+# Skapa ett filter baserat på nyckelorden för vården
+filter_varden = df['headline'].str.contains('|'.join(gemensamma_keywords), case=False, na=False)
 
-print("-------------------------- SHAPE --------------------------")
-print(df.shape)
+# Skapa subset med historiska jobbannonser som innehåller minst ett nyckelord i description.text
+subset = df[filter_varden]
 
-print("-------------------------- FIRST 10 ROWS --------------------------")
-print(df.head(10))
+# Välj ut angivna kolumner
+subset = subset[['application_deadline', 'headline', 'number_of_vacancies', 'publication_date', 
+                 'description.text', 'duration.label', 'employer.name', 
+                 'working_hours_type.label', 'workplace_address.municipality_code']]
 
-print("-------------------------- LAST 10 ROWS --------------------------")
-print(df.tail(10))
+# Konvertera kolumnen "number_of_vacancies" till numerisk form
+subset['number_of_vacancies'] = pd.to_numeric(subset['number_of_vacancies'], errors='coerce')
 
-print("-------------------------- LOOK AT COLUMN PER NAME ('headline') --------------------------")
-print(df["headline"])
+# Printa de första 5 raderna
+print(subset.head())
 
-print("-------------------------- ACCESS DATA PER INDEX/'iloc (ROW)' --------------------------")
-print(df.iloc[1])
+# Ta bort rader där antalet lediga platser är mer än 50
+subset = subset[subset['number_of_vacancies'] <= 50]
 
-print("-------------------------- DISPLAY STRINGS IN A COLUMN (ex. specific jobs 'sjuksköterska, undersköterska' in column 'headline') '.isin()' --------------------------")
-yrken = ["sjuksköterska", "undersköterska"]
-print(df[df["headline"].isin(yrken)])
+# Ta bort alla exakta dubbletter i hela subsetet
+subset.drop_duplicates(inplace=True)
 
-print("-------------------------- DISPLAY ROWS WHERE THE COLUMN CONTAINS A SPECIFIC STRING ('sjuk') '.str.contains()' --------------------------")
-print(df[df["headline"].str.contains("sjuk")])
-
-print("-------------------------- DISPLAY SELECTED ROWS FROM A FILTERED DF ('sjuk') '.str.contains()' --------------------------")
-sorterade = df[df["headline"].str.contains("sjuk")]
-    # axis = 1 för att filtrera bland kolumner, 0 för rader 
-print(sorterade.filter(items = ["working_hours_type", "headline"], axis = 1))
+# Ta bort rader där beskrivningstexten är saknad
+subset = subset.dropna(subset=['description.text'])
+print(subset)
 
 
-#print("-------------------------- DISPLAY SELECTED ROWS FROM A FILTERED DF AND ONLY SHOWS 'last_publication_date' after 2006-12-31  ('sjuk') '.str.contains()' --------------------------")
-#print(sorterade[sorterade["last_publication_date"] > "2006-12-31"])
-    # sort_values gör att man kan sortera för värden inom en kolumn, här med datum
-    # för mer info om sortering se: https://youtu.be/kB7FV-ijdqE?si=B8kDfABae9009dXR&t=562
-#print(sorterade[sorterade["last_publication_date"] > "2006-12-31"].sort_values(by = "last_publication_date"))
-
-
-#print("-------------------------- SORT FOR SJUK AND SETS THE INDEX AS EMPLOYER --------------------------")
-
-#indexerat = sorterade_2.set_index("employer")
-#print(indexerat)
-
-
-# Skriver ut annonserna med index 0, 1, 2 utifrån den sorterade och indexerade listan ovan
-sorterade_2 = df[df["headline"].str.contains("sjuk")]
-print(sorterade_2.iloc[1], sorterade_2.iloc[0], sorterade_2.iloc[2])
-
-
-
-# Räknar ut antalet värden inom en column, label, och skriver ut hur många det finns av varje
-working_hours_type = df["working_hours_type.label"]
-print(working_hours_type.value_counts())
-
-# Skriver ut all data för kolumnen working_hours_type där "sjuk" omnämns i headline  
-sorterade_2 = df[df["headline"].str.contains("sjuk", "vård")]
-test_1 = sorterade_2["working_hours_type.label"]
-print(test_1)
-print(test_1.value_counts())
-
-# gör om ovanstående till en lista och skriver endast ut "label" för denna kolumns data
-list_of_working_hours = test_1.tolist()
-print(list_of_working_hours)
-#for listing in list_of_working_hours:
-#    print()
-
-print(len(list_of_working_hours))
-#print(df)
-text_labels = sorterade_2["description.text"]
-list_of_labels = text_labels.tolist()
+subset_description = subset["description.text"]
+list_of_labels = subset_description.tolist()
 print(len(list_of_labels))
 
-dict_of_all_words = {}
-list_of_all_words = []
-for label in list_of_labels:
-    
-    print(label)
-    print(f"-- Antal ord i annonsen: ")
-    get_list_of_words(label)
-    for new_dict in get_list_of_words(label):
-        dict_of_all_words[new_dict["char"]] = new_dict["num"]
-        list_of_all_words.append(f"Ordet: '{new_dict["char"]}' fanns {new_dict["num"]} gånger.")
-        print(f"Ordet: '{new_dict["char"]}' fanns {new_dict["num"]} gånger.")
-    print("--------------------------------------------------- NEXT")
-sort_dict_of_words = sorted(dict_of_all_words.items(), key = lambda x:x[1], reverse=False)
 
 
+def sorting_dict(dict):
+    unsorted_list_of_tuples = sorted(dict.items())
+    return sorted(unsorted_list_of_tuples, key=itemgetter(1))
 
-print(f"SUPERLISTA: {sort_dict_of_words}")
-print(len(sort_dict_of_words))
+def splitting_and_counting(list_of_text):
+    giant_list = []
+    for list in list_of_text:
 
+        splitted_text = list.split()
+        giant_list += splitted_text
+    return giant_list
 
-    #print(get_list_of_words(label))
-    
-    #for dict in get_list_of_words(label):         
-    #    print(f"The '{dict["char"]}' character was found {dict["num"]} times.")
-    #    print("--------------------------------------------------- NEXT")
-
-#print(text_labels[907])
-#for label in text_labels:
-#    print(label)
-
-
-
-"""
-with open (("dataset/subset.json"), "r") as file:
-    data = json.load(file)
-
-jobtech_dataset = pd.json_normalize(data)
-print(jobtech_dataset.columns)
-"""
+def dict_string_and_num(list):
+    giant_dict = dict()
+    for word in list:
+        lowered_word = word.lower()
+        if lowered_word not in stopwords_list and lowered_word.isalpha():
+            giant_dict[lowered_word] = giant_dict.get(lowered_word, 0) + 1
+    return sorting_dict(giant_dict)
 
 
+total_number = dict_string_and_num(splitting_and_counting(list_of_labels)) 
 
-#print(data)
+
+print(total_number)
+print(len(total_number))
 
 
+pattern_for_word = re.compile(r"(\b\S+\s\S+\s\S+\s\S+\s\S+\s\S+\s\S+\s\S+\s\S+\s\S+\s)?augmented reality(\s\S+\s\S+\s\S+\s\S+\s\S+\s\S+\s\S+\s\S+\s\S+\s\S+\b)?")
